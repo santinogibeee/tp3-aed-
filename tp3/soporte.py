82577e63128f89856f571ce5d53311a0f7f0a6ad
santigno1k04 <santinoestudiantegibe@gmail.com>
2026-09-15 17:33:54 -0300

    Actualiza TP3 y agrega CSV de prueba

--- tp3/soporte.py
+++ tp3/soporte.py
@@ -1,5 +1,5 @@
 class Tratamiento: 
-    def __init__(self, dni, nombre, apellido, icd10, montobase, complejidad, algoritmoid, letra, bloque, montofinal):
+    def __init__(self, dni, nombre, apellido, icd10, montobase, complejidad, algoritmoid):
         self.dni = dni
         self.nombre = nombre
         self.apellido = apellido
@@ -7,24 +7,33 @@ class Tratamiento:
         self.montobase = montobase
         self.complejidad = complejidad
         self.algoritmoid = algoritmoid
-        self.letra = letra
-        self.bloque = bloque
-        self.montofinal = montofinal
     
     def __str__(self): # ACOMODAR LOS VALORES {} EN RELACION AL ESPACIO A OCUPAR...
+        
         r = "DNI: {:<5} - Nombre: {:<5}- Apellido: {:<5}".format(self.dni, self.nombre, self.apellido)
-        r += " - ICD10: {:<5} - Monto Base: {:<5} - Complejidad: {:<5}".format(self.icd10, self.montobase, self.complejidad)
-        r += "- Algoritmo: {:<5} - Letra: {:<5} - Bloque: {:<5} - Monto Final: {:<5}".format(self.algoritmoid, self.letra, self.bloque, self.montofinal)
+        r += " - ICD10: {:<5} - Monto Base: {:<5}".format(self.icd10, self.montobase)
+        r += " - Complejidad: {:<5} - Algoritmo: {:<5}".format(self.complejidad,self.algoritmoid)
         
         return r
-    
-    
+
+def procesarlinea(linea):
+    partes = linea.split(",") # Split lo devuelve en partes, separa en una lista las partes de la cadena segun el criterio ","
+    dni = int(partes[0])
+    nombre = partes[1]
+    apellido = partes[2]
+    icd10 = partes[3]
+    monto = float(partes[4])
+    complejidad = partes[5]
+    algoritmo = int(partes[6])
+
+    t = Tratamiento(dni, nombre, apellido, icd10, monto, complejidad, algoritmo)
+    return t
+
+
+
+
+
+        
 if __name__ == "__main__":
-    p = Tratamiento()
-    print(p)
-        
-
-
-        
-        
-        
+    linea = "new.csv"
+    procesarlinea(linea)
--- tp3/system.py
+++ tp3/system.py
@@ -1,4 +1,39 @@
 from soporte import *
 import random
+import os.path
+
+def leerarchivo():
+    # Verificacion de que el archivo existe:
+    tratamientos = []
+    archivo = "new.csv"
+    if not os.path.exists(archivo):
+        print("El archivo existe!")
+        return
+    m = open(archivo, "rt")
+    m.readline()
+
+    for linea in m:
+        if linea[-1] == "\n":
+            linea = linea[:-1]
+        t = procesarlinea(linea)
+
+        # CUIDADO!
+        tratamientos.append(t)
 
 
+
+
+
+    m.close()
+    return tratamientos
+
+
+
+def principal():
+    # Procesar archivo csv
+    tratamientos = leerarchivo()
+    print("r1.1: ", len(tratamientos))
+    print("r1.2: ")
+
+if __name__ == "__main__":
+    principal()
